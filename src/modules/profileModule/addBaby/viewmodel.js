53c6028ca42f1c1cb0f67a8be87dbda110546432
Malintha Adikari <mailtoadikari@gmail.com>
2012-09-29 20:07:05 +0530

    Added  UI items for addbaby

--- src/modules/profileModule/addBaby/viewmodel.js
+++ src/modules/profileModule/addBaby/viewmodel.js
@@ -1,10 +1,59 @@
 define([], function () {
 
     var ViewModel = function (moduleContext) {
-
+				
         var self = this;
-        //Implement the viewmodel here
+         var source = [
+                    "Male","Female",
+		        ];
+		        var sourceCriStat = [
+                    "Critical","In Risk","Normal",
+		        ];
 
+                // Create a jqxDropDownList
+                $("#jqxdropdowSex").jqxDropDownList({ source: source, selectedIndex: 0, width: '141px', height: '20px' });
+               
+                
+                $('#jqxdropdowSex').bind('select', function (event) {
+                    var args = event.args;
+                    var item = $('#jqxdropdowSex').jqxDropDownList('getItem', args.index);
+                    alert('Selected: ' + item.label);
+                });
+                
+                $("#jqxdropdownCritical").jqxDropDownList({ source: sourceCriStat, selectedIndex: 0, width: '141px', height: '20px' });
+               
+                
+                $('#jqxdropdownCritical').bind('select', function (event) {
+                    var args = event.args;
+                    var item = $('#jqxdropdownCritical').jqxDropDownList('getItem', args.index);
+                    alert('Selected: ' + item.label);
+                });
+                
+                   $("#jqxdatetimeinput").jqxDateTimeInput({ width: '141px', height: '20px' });
+          // init buttons.
+          $("#getDateButton").jqxButton();
+          $("#setDateButton").jqxButton();
+          $("#jqxButtonNew").jqxToggleButton({ width: '50px', height: '20px', toggled: true});
+          // set new date.
+          $("#setDateButton").click(function () {
+              var date = new Date();
+              date.setFullYear(2012, 5, 6);
+              $('#jqxdatetimeinput').jqxDateTimeInput('setDate', date);
+          });
+          // get date.
+          $("#getDateButton").click(function () {
+              var date = $('#jqxdatetimeinput').jqxDateTimeInput('getDate');
+              alert(date);
+          });
+                  
+                  
+       this.renderComponents=function(){
+			//$("#myButton").jqxButton({ width: '120px', height: '35px', theme: 'darkblue'});
+		
+                
+		
+		}
+		
     };
     
     return ViewModel;
