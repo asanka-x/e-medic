04e306259aac4b51f447887df3d329fb2998e3c7
Asanka <asankanissanka@hotmail.com>
2012-09-30 18:53:24 +0530

    added maps

--- src/modules/locationServices/googleMaps/viewmodel.js
+++ src/modules/locationServices/googleMaps/viewmodel.js
@@ -1,11 +1,20 @@
-define([], function () {
+define([], function() {
 
-    var ViewModel = function (moduleContext) {
+	var ViewModel = function(moduleContext) {
 
-        var self = this;
-        //Implement the viewmodel here
+		var self = this;
+		var map1, latlng1, options1;
+		this.initialize = function() {
+			latlng1 = new google.maps.LatLng(40.716948, -74.003563);
+			options1 = {
+				zoom : 14,
+				center : latlng1,
+				mapTypeId : google.maps.MapTypeId.ROADMAP
+			};
+			map1 = new google.maps.Map(document.getElementById("map1"), options1);
+		};
 
-    };
-    
-    return ViewModel;
+	};
+
+	return ViewModel;
 });
