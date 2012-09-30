866c96b5c71c176eff84406033b64ca8826b6885
Sameera Madusanka Perera <smperera77@gmail.com>
2012-09-30 07:26:04 +0530

    Family Ragistration added

--- src/modules/profileModule/addFamily/viewmodel.js
+++ src/modules/profileModule/addFamily/viewmodel.js
@@ -1,11 +1,55 @@
-define([], function () {
+define([], function() {
 
-    var ViewModel = function (moduleContext) {
+	var ViewModel = function(moduleContext) {
 
-        var self = this;
-        //Implement the viewmodel here
+		this.wifeName = ko.observable("");
+		this.husbandName = ko.observable("");
+		this.marriedAge = ko.observable("");
+		this.education = ko.observable("");
+		this.address = ko.observable("");
+		this.occupation = ko.observable("");
 
-    };
-    
-    return ViewModel;
+		var self = this;
+		this.renderComponents = function() {
+			//$("#myButton").jqxButton({ width: '120px', height: '35px', theme: 'darkblue'});
+			$("#dtiBirthDate").jqxDateTimeInput({ width : '162px', height : '25px', theme :'shinyblack'});
+			
+			$("#jqxscrollbar").jqxScrollBar({ width: 250, theme:'shinyblack', height: 18 });
+			$("#jqxverticalscrollbar").jqxScrollBar({ width: 18, height: 250,theme :'shinyblack', vertical: true });
+			
+			$("#jqxYesRadioButton").jqxRadioButton({ width: 120,theme :'shinyblack', height: 25 });
+            $("#jqxNoRadioButton").jqxRadioButton({ width: 120, height: 25,theme :'shinyblack' });
+            // bind to change event.
+            $("#jqxYesRadioButton").bind('change', function (event) {
+                var checked = event.args.checked;
+                //alert('jqxYesRadioButton checked: ' + checked);
+            });
+            $("#jqxNoRadioButton").bind('change', function (event) {
+                var checked = event.args.checked;
+                //alert('jqxNoRadioButton checked: ' + checked);
+            });
+            
+            $("#dtiRagisterDate").jqxDateTimeInput({ width : '162px', height : '25px', theme :'shinyblack'});
+			//$("#jqxverticalscrollbar").bind('valuechanged', function (event) {
+            //  $('#VerticalDiv').html('Vertical (' + parseInt(event.currentValue) + ')');
+            //});
+			// init buttons.
+			//$("#getDateButton").jqxButton();
+			//$("#setDateButton").jqxButton();
+			// set new date.
+			//$("#setDateButton").click(function() {
+			//	var date = new Date();
+			//	date.setFullYear(2012, 5, 6);
+			//	$('#jqxdatetimeinput').jqxDateTimeInput('setDate', date);
+			//});
+			// get date.
+			//$("#getDateButton").click(function() {
+			//	var date = $('#jqxdatetimeinput').jqxDateTimeInput('getDate');
+			//	alert(date);
+			//});
+		};
+
+	};
+
+	return ViewModel;
 });
