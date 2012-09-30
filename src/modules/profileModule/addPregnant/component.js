be1d5136fcc842e4d9109022914380582cb06220
gayanwiharagoda <gayanwiharagoda@gmail.com>
2012-09-30 08:23:41 +0530

    pregnent mother view has commited

--- src/modules/profileModule/addPregnant/component.js
+++ src/modules/profileModule/addPregnant/component.js
@@ -12,7 +12,7 @@ define(['Boiler', 'text!./view.html', './viewmodel', 'text!./style.css', 'i18n!.
 				ko.applyBindings(vm, panel.getDomElement());
 			}
 			
-			vm.setNewDepartmentName(params.name);
+			//vm.renderComponents();
 			panel.show();
 		}
 
